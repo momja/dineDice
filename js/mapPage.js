8ce184e41cb06937f0874cc851e78048a47b21dc
Max Omdal <mjomdal@gmail.com>
2017-06-30 15:12:02 -0500

    changing search radius with slider

--- js/mapPage.js
+++ js/mapPage.js
@@ -7,19 +7,20 @@ var nearbyPlaces = [];
 
 // code starts here. begins by making map, finding your current location, and starts the search
 function initMap() {
+
+  getInformation();
+
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 13,
     styles: [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}],
     disableDefaultUI: true
   });
-
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
       var pos = {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
-
       map.setCenter(pos);
 
       searchRadius = 2000;
@@ -34,7 +35,6 @@ function initMap() {
             center: pos,
             radius: searchRadius
         };
-
         // Add the circle for this city to the map.
         cityCircle = new google.maps.Circle(radiusOptions);
 
@@ -44,15 +44,17 @@ function initMap() {
       handleLocationError(true, infoWindow, map.getCenter());
     });
   }
-
   else {
     // Browser doesn't support Geolocation
     handleLocationError(false, infoWindow, map.getCenter());
   }
-
   infoWindow = new google.maps.InfoWindow();
   service = new google.maps.places.PlacesService(map);
+}
 
+function getInformation() {
+  searchRadius = sessionStorage.getItem('radius') || 5000;
+  var price = sessionStorage.getItem('price') || 2;
 }
 
 function performSearch(pos) {
