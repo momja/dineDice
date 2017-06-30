699202626c2132fc88c242f1f8b19e232bdc956c
Max Omdal <mjomdal@gmail.com>
2017-06-30 16:55:26 -0500

    moved sliders from js to html

--- js/index.js
+++ js/index.js
@@ -1,29 +1,4 @@
 
-function initInputPage() {
-  createDistanceSlider();
-  createPriceSlider();
-}
-
-// creates a distance slider with minimum range of 1 KM and max 20 KM
-function createDistanceSlider() {
-  var x = document.createElement("INPUT");
-  x.setAttribute("type", "range");
-  x.id = "distanceRange";
-  x.min = 1000; // meters
-  x.max = 20000; // meters
-  document.body.appendChild(x);
-}
-
-// creates a price slider, with 3 levels: $, $$, and $$$
-function createPriceSlider() {
-  var x = document.createElement("INPUT");
-  x.setAttribute("type", "range");
-  x.id = "priceRange";
-  x.min = 0;
-  x.max = 2;
-  document.body.appendChild(x);
-}
-
 // set the information so the next page can make use of them, and they can be remembered
 function setInformation() {
   var radius = document.getElementById("distanceRange").value;
